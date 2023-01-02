dfe702ec8e3c627e7ab85158420c638a7f423bc2
dunossauro <mendesxeduardo@gmail.com>
2023-01-02 09:36:13 -0300

    Simplificando código para geração das escalas

--- gerador_de_escalas.py
+++ gerador_de_escalas.py
@@ -8,14 +8,6 @@ console = Console()
 NOTES = 'C C# D D# E F F# G G# A A# B'.split()
 
 
-class Scale:
-    def __init__(self, scale):
-        self.scale = scale
-
-    def __getitem__(self, pos):
-        return self.scale[pos]
-
-
 class Notes:
     def __init__(self, scale, tonic):
         self.scale = scale
@@ -33,43 +25,41 @@ class Notes:
 
 
 scales = {
-    'major': Scale((0, 2, 4, 5, 7, 9, 11, 0)),
-    'minor': Scale((0, 2, 3, 5, 7, 8, 10, 0)),
+    'major': {
+        'notes': (0, 2, 4, 5, 7, 9, 11),
+        'chords': ('I', 'ii', 'iii', 'IV', 'V', 'vi', 'vii°')
+    },
+    'minor': {
+        'notes': (0, 2, 3, 5, 7, 8, 10),
+        'chords': ('i', 'ii°', 'III', 'iv', 'v', 'VI', 'VII'),
+    }
 }
 
 
-def create_table(scale, tonic):
+def create_table(scale, tonic, chords):
     if tonic != 'all':
         table = Table(title=f'Escala {scale} de {tonic}', show_lines=True)
     else:
         table = Table(title=f'Escala {scale}', show_lines=True)
 
-    table.add_column('Iº Tônica', justify='center')
-    table.add_column('IIº - T', justify='center')
-    table.add_column('IIIº - T', justify='center')
-    table.add_column('IVº - ST', justify='center')
-    table.add_column('Vº - T', justify='center')
-    table.add_column('VIº - T', justify='center')
-    table.add_column('VIIº - T', justify='center')
-    table.add_column('Iº - T', justify='center')
+    for chord in chords:
+        table.add_column(chord, justify='center')
 
     return table
 
 
-def main(scale: str = Argument('major'), tonic: str = Argument('all')):
-    table = create_table(scale, tonic)
+def main(
+        scale: str = Argument('major'),
+        tonic: str = Argument('all'),
+):
+    _scale = scales[scale]
+    table = create_table(scale, tonic, _scale['chords'])
 
-    match scale, tonic:
-        case 'major', 'all':
-            _scale: Scale = scales['major']
-            for _tonic in NOTES:
-                table.add_row(*Notes(_scale, _tonic).notes)
-        case 'minor', 'all':
-            _scale: Scale = scales['minor']
-            for _tonic in NOTES:
-                table.add_row(*Notes(_scale, _tonic).notes)
-        case _, _:
-            table.add_row(*Notes(scales[scale], tonic).notes)
+    if tonic == 'all':
+        for _tonic in NOTES:
+            table.add_row(*Notes(_scale['notes'], _tonic).notes)
+    else:
+        table.add_row(*Notes(_scale['notes'], tonic).notes)
 
     console.print(table)
 
